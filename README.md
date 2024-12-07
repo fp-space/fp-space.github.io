0526fc55e9343b34d757c367b39b0141b12f7fe1
TheFoxFairy <1479852727@qq.com>
2024-12-07 16:27:23 +0800

    chore(项目): 初始化

---
+++ README.md
@@ -0,0 +1,3 @@
+Mini Note
+
+采用 Rust 编程语言，选择 Yew 框架
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use console_log::init_with_level;
+use log::Level;
+use yew::{function_component, html, Html};
+
+
+#[function_component]
+fn App() -> Html {
+    html! {
+        <div>
+            <h1>{"Hello Mini Note"}</h1>
+        </div>
+    }
+}
+
+fn main() {
+    // 初始化日志
+    init_with_level(Level::Info).expect("failed to initialize logger");
+
+    yew::Renderer::<App>::new().render();
+}
