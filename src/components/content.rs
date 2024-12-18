a18670cfc3c8a68ca4ce34527681ad540c621d01
sehnsucht <tece@sehnsucht.top>
2024-12-18 17:22:03 +0800

    feat: 新增markdown解析

--- src/components/content.rs
+++ src/components/content.rs
@@ -1,24 +1,15 @@
 // 内容展示区域
 
 use yew::prelude::*;
+// markdown 渲染库
+use yew_markdown::Markdown;
+
+const MARKDOWN_SOURCE: &str =
+    include_str!("../../public/storage/StudyNotes/Java编程/07-云原生/01-Docker篇.md");
 
 #[function_component(MainContent)]
 pub fn main_content() -> Html {
     html! {
-        <div class="px-40 py-10">
-            <article class="prose max-w-none">
-                <h1> {"Garlic bread with cheese: What the science tells us"} </h1>
-                <p>
-                    {"For years parents have espoused the health benefits of eating garlic bread with cheese to their
-                    children, with the food earning such an iconic status in our culture that kids will often dress
-                    up as warm, cheesy loaf for Halloween."}
-                </p>
-                <p>
-                    {"But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases
-                    springing up around the country."}
-                </p>
-            </article>
-        </div>
-
+        <Markdown src={MARKDOWN_SOURCE}/>
     }
 }
